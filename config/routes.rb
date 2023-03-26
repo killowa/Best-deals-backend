a4f170c5a753a62e9d1129e201ed7d60be82e776
Omar Sherif Khalil <omarsherif74@hotmail.com>
2023-03-26 23:31:09 +0200

    Added searches and websites routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :products
+      # post "scrape", to: "websites#scrape"
+      get "searches", to: "search_keywords#index"
+      get "websites", to: "websites#list_websites"
+    end
+  end
+
+  # resources :products
+
+
 end
