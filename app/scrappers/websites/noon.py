7b7a2392663c69c8e3341f418c80d96f80ca99a0
TheodoraGeorge <es-Theodora.Riad23@alexu.edu.eg>
2023-03-27 21:37:16 +0200

    noon scrap function

---
+++ app/scrappers/websites/noon.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from product import Product
+
+
+def scrap(search_keys):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    # PATH = "/usr/local/bin/chromedriver"
+    # driver = webdriver.Chrome(PATH)
+    driver.get('https://www.noon.com/egypt-en/')
+    driver.implicitly_wait(10)
+
+    search = driver.find_element(By.CSS_SELECTOR, 'input[id]')
+    search.send_keys(search_keys)
+    search.send_keys(Keys.RETURN)
+
+    names=[]
+    prices=[]
+    links=[]
+    rates=[]
+    peopleRates=[]
+    products=[]
+    #CSS SELECTORS
+    # div[data-qa='product-name']
+    # strong[class='amount']
+    # span[class='sc-5e739f1b-0 gEERDr wrapper productContainer  ']>a
+    # div[class='sc-61515602-0 czLWQH']
+
+    productNames = driver.find_elements(By.CSS_SELECTOR,"div[data-qa='product-name']")
+    productPrices = driver.find_elements(By.CSS_SELECTOR,"strong[class='amount']")
+    productHref = driver.find_elements(By.CSS_SELECTOR,"span[class='sc-5e739f1b-0 gEERDr wrapper productContainer  ']>a")
+    productRates = driver.find_elements(By.CSS_SELECTOR,"div[class='sc-61515602-0 czLWQH']")
+
+    for name in productNames:
+        names.append(name.get_attribute("title"))
+    for price in productPrices:
+        prices.append(price.text)  
+    for link in productHref:
+        links.append(link.get_attribute("href"))
+    for r in productRates:
+        rate=r.text.split('\n', 1)[0]
+        if rate =='':
+            rate=0
+            numOfRates=0
+        else:
+            numOfRates=r.text.split('\n', 1)[1]
+        rates.append(rate)
+        peopleRates.append(numOfRates)
+
+    products=zip(names,prices,links,rates,peopleRates)
+    for product in list(products):
+        print(product)
+        print('-'*50)
+
+    for i in range(len(names)):
+        product = Product(float(prices[i]), float(rates[i]),int(numOfRates[i]), 0,names[i], 'noon',links[i])
+        products.append(product)
+
+
+    return products
