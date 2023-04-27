531a217cb6d61f9189add2b2b4a9f1f7a658268d
Omar Sherif Khalil <omarsherif74@hotmail.com>
2023-04-27 03:36:37 +0200

    Rollback db transactions upon failure

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -20,35 +20,42 @@ class Api::V1::ProductsController < ApplicationController
         return json_response({ error: "Please enter a search item" })
       end
   
-      # Create a new search item in searches table
-      @search_keyword = SearchKeyword.create(search_key: params[:search_key], website_name: "all")
-      @search_keyword.save!
-  
-      scraped_products = `python3 app/scrappers/main.py "#{params[:search_key]}"`
-      # binding.pry
+      ActiveRecord::Base.transaction do
 
-      # replace single quotes with double quotes
-      scraped_products = scraped_products.gsub(/'/, '"')
-      if scraped_products.empty?
-        return json_response({ message: "No results found" })
-      end
-      @scraped_products = JSON.parse(scraped_products)
-
-      # Create an array of Product objects from the parsed JSON data
-      @scraped_products.each do |item|
+        # Create a new search item in searches table
+        @search_keyword = SearchKeyword.create!(search_key: params[:search_key], website_name: "all")
+        @search_keyword.save!
+    
+        scraped_products = `python3 app/scrappers/main.py "#{params[:search_key]}"`
         # binding.pry
-        @new_scraped_product = Product.new(
-          name: item['header'],
-          price: item['price'],
-          link: item['link'],
-          rating: item['rate'],
-          # score: item['score'],
-          reviews_count: item['reviewsCount'],
-          img_url: item['imageUrl'],
-          source: item['source']
-        )
-        @new_scraped_product.save!
-      end
+
+        # replace single quotes with double quotes
+        scraped_products = scraped_products.gsub(/'/, '"')
+        if scraped_products.empty?
+          return json_response({ message: "No results found" })
+        end
+        @scraped_products = JSON.parse(scraped_products)
+
+        # Create an array of Product objects from the parsed JSON data
+        @scraped_products.each do |item|
+          # binding.pry
+          @new_scraped_product = Product.new(
+            name: item['header'],
+            price: item['price'],
+            link: item['link'],
+            rating: item['rate'],
+            # score: item['score'],
+            reviews_count: item['reviewsCount'],
+            img_url: item['imageUrl'],
+            source: item['source']
+          )
+          # @new_scraped_product.search_keyword = @search_keyword # associate the search keyword with each product
+          
+          @new_scraped_product.save!
+        end
+    end
+      # @scraped_products.save_with_keyword(params[:search_key],"all") # Call the model method with the search keyword
+      # redirect_to @scraped_products
 
   
       json_response(@scraped_products)
