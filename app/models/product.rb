dd8c41088c1ebe6064817aaef2dc18e3419e7385
Omar Sherif Khalil <omarsherif74@hotmail.com>
2023-04-27 03:36:53 +0200

    Bug fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ApplicationRecord
     # belongs_to :website
+    # after_create :create_search_keyword
+
+    # def create_search_keyword(search_keyword, website_name)
+    #     SearchKeyword.create(search_key: search_keyword, website_name: website_name)
+    # end
+
+    # def save_with_keyword(search_keyword)
+    #     self.search_keyword = search_keyword
+    #     save!
+        
+    #     SearchKeyword.create(keyword: search_keyword)
+    #   end
 
 
 end
--- app/models/search_keyword.rb
+++ app/models/search_keyword.rb
@@ -1,4 +1,4 @@
 class SearchKeyword < ApplicationRecord
-    validates :search_key, uniqueness: { case_sensitive: false, message: "search key previously searched" }
+    validates :search_key, uniqueness: { case_sensitive: false, message: "previously searched!" }
 
 end
