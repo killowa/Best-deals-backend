6ba02f90790f0b0638e16e076274c45627ba41cd
Omar Sherif Khalil <omarsherif74@hotmail.com>
2023-04-28 18:17:04 +0300

    seed database with websites

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,11 @@
 class ApplicationController < ActionController::API
     include Response
-    before_action :init_websites
+    # before_action :init_websites
 
 
-    def init_websites
-      # This method will be called before any other actions in the controller
-
-      # Create entries for every website in the database
-      # This is so that we can easily add more websites to scrape from
-      # in the future
+    def index
       @websites = Website.all
-      if @websites.length == 0
-        @jumia = Website.create(name: "Jumia", url: "https://www.jumia.com.eg/catalog/?q=")
-        @amazon = Website.create(name: "Amazon", url: "https://www.amazon.eg/-/en/")
-        @noon = Website.create(name: "Noon", url: "https://www.noon.com/egypt-en/")
-      end
+      json_response(@websites)
     end
 
     rescue_from ActiveRecord::RecordNotFound do |exception|
