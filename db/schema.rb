778c0f998f8490a88b6d52d90f4e3414333b3cfa
martina mamdouh <martinadouha2000@gmail.com>
2023-05-05 16:57:18 +0300

    favorites-table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_012341) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_214820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
