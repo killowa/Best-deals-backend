c85ad6d070d2124d1e6a159794f641af10b31351
martina mamdouh <martinadouha2000@gmail.com>
2023-05-27 22:34:16 +0300

    Merge history-backend-to wishlist-backend

--- app/controllers/api/v1/histories_controller.rb
+++ app/controllers/api/v1/histories_controller.rb
@@ -4,10 +4,11 @@ module Api
         class Api::V1::HistoriesController < ApplicationController
             before_action :authenticate_request
             after_action :limit_histories
-            MAX_HISTORIES = 8
+            MAX_HISTORIES = 100
             
             def index
-                @histories = current_user.histories
+                # show the history database table entries in descending order
+                @histories = current_user.histories.order(created_at: :desc)
                 render json: @histories
             end
             
@@ -16,9 +17,12 @@ module Api
                 if !params[:product_id]&.present?
                     return json_response({ error: "Please enter product id" })
                 end
-
+                # find the product with the given ID, and deletes the user's previous record of that product if it exists.
+                @product = Product.find(params[:product_id])
+                old_record = current_user.histories.where(product_id: @product.id).last
+                old_record.destroy if old_record.present?
                 
-
+                # create a new history record for the user with the current product ID
                 @history = current_user.histories.new(
                     product_id: params[:product_id]
                 )
@@ -28,8 +32,10 @@ module Api
                     render json: @history.errors, status: :unprocessable_entity
                 end
             end
+            
 
             private
+            
 
             def limit_histories
                 count = current_user.histories.count
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,5 +1,4 @@
 class History < ApplicationRecord
     belongs_to :user
     belongs_to :product
-    validates :product_id, uniqueness: { message: "previously searched!" }
 end
