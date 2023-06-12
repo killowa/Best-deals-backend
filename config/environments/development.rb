32f9d98093c0c75573503fbacaf07d1923f9b0e0
martina mamdouh <martinadouha2000@gmail.com>
2023-06-12 19:23:57 +0300

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,8 +53,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # This will allow any device on the network to access your Rails server.
-  config.hosts.clear
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
