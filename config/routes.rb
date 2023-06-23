c5f90d3df995e67bcbdfecd2f79a2ff60b68ccaf
Omar Sherif Khalil <omarsherif74@hotmail.com>
2023-06-23 17:20:12 +0300

    Cleaned code

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products, :histories, :favorites
+      resources :products, :histories, :favorites, :ratings
       resources :websites, only: :index
       resources :users, only: :create do
         collection do
@@ -16,28 +16,7 @@ Rails.application.routes.draw do
       end
 
       get "searches", to: "search_keywords#index"
-      get "websites", to: "websites#list_websites"
-      resources :favorites do
-        collection do
-          post 'create'
-          get "favorites",to:"favorites#index"
-          post 'destroy'
-        end
-      end
-       
-      resources :ratings do
-        collection do
-          post 'create'
-          get 'ratings', to: 'ratings#index'
-        end
-      end
 
-      resources :histories do
-          collection do
-          get 'histories', to: 'histories#index'
-          post 'create'
-          end
-        end
       end
   end
 
