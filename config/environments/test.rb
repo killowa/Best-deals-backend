37a1139e53cd6d5ce236a8e215bbba5b7c05098f
martina mamdouh <martinadouha2000@gmail.com>
2023-07-01 17:12:29 +0300

    Merge branch 'master' of https://github.com/killowa/Best-deals-backend into wishlist-backend

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,18 @@ Rails.application.configure do
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     address: ENV['SMTP_ADDRESS'],
+     port: ENV['SMTP_PORT'],
+     user_name: ENV['SMTP_USERNAME'],
+     password: ENV['SMTP_PASSWORD'],
+     authentication: 'plain',
+     enable_starttls_auto: true
+   }
+  
+  
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
   # system, or in some way before deploying your code.
