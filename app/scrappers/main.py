8f680783adad797c87a69008838db83ccab5dade
killowa <mazensallam75@gmail.com>
2023-07-01 09:57:33 +0300

    Test

--- app/scrappers/main.py
+++ app/scrappers/main.py
@@ -33,18 +33,18 @@ products = []
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
+options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 # options.add_argument('--remote-debugging-port=9222')
 
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 
-products += amazon_scrap(driver, SEARCH_KEYS, num_of_products=10)
+# products += amazon_scrap(driver, SEARCH_KEYS, num_of_products=10)
 
-driver.quit()
+# driver.quit()
 
-options.add_argument(f'--proxy-server=http://41.65.227.103:1976')
+# options.add_argument(f'--proxy-server=http://41.65.227.103:1976')
 driver = webdriver.Chrome(service=Service(
     ChromeDriverManager().install()), options=options)
 
@@ -55,7 +55,7 @@ driver = webdriver.Chrome(service=Service(
 
 # n is the maximum number of products to scrape from each website
 for scrappingFile in scrappingFiles:
-    if scrappingFile.find('amazon') != -1: continue
+    # if scrappingFile.find('amazon') != -1: continue
     products += __import__(scrappingFile, fromlist=['scrap']).scrap(
         driver, SEARCH_KEYS, num_of_products=10)
 
